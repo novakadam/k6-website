904096d25639f6d3dd207fa8b1e851f366a64811
Ádám Novák <novakadam@192.168.1.9>
2025-11-22 17:49:45 +0100

    contact git push
    git add .
    git commit -m contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -84,7 +84,7 @@ import { toast } from '@/components/ui/use-toast';
                     <Mail className="w-6 h-6 text-gray-400 mt-1" />
                     <div>
                       <p className="font-medium mb-1"><EditableField as="span" isEditable={isEditMode} initialValue="E-mail" /></p>
-                      <EditableField as="a" href="mailto:info@k6studios.com" className="text-gray-600 hover:text-gray-900" isEditable={isEditMode} initialValue="info@k6studios.com" />
+                      <EditableField as="a" href="mailto:info@k6alkotohaz.hu" className="text-gray-600 hover:text-gray-900" isEditable={isEditMode} initialValue="info@k6alkotohaz.hu" />
                     </div>
                   </div>
 
